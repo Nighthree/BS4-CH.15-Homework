927a245da1c85ab61c9fffa1d8f74260fe4595ad
Nighthree <jenny605328@gmail.com>
2019-06-27 17:14:18 +0800

    完成

--- js/all.js
+++ js/all.js
@@ -4,4 +4,20 @@ $(document).ready(function () {
     $(this).find('.heart-border').toggleClass('show');
     $(this).find('.heart-full').toggleClass('show');
   });
+
+  $('.e-bill').click(function (e) { 
+    e.preventDefault();
+    $(this).addClass('active');
+    $('.post-bill').removeClass('active');
+    $('.e-bill-address').addClass('open');  
+    $('.post-bill-address').removeClass('open');
+  });
+
+  $('.post-bill').click(function (e) { 
+    e.preventDefault();
+    $(this).addClass('active');
+    $('.e-bill').removeClass('active');
+    $('.post-bill-address').addClass('open');  
+    $('.e-bill-address').removeClass('open');
+  });
 });
